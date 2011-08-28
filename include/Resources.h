5d7dc94ef3840724410d1b360cd98c56f101b975
Patrick Tierney <patrick.l.tierney@gmail.com>
2011-08-28 14:06:07 +0100

    Removed old resource code that's no longer relevant.

--- include/Resources.h
+++ include/Resources.h
@@ -7,7 +7,6 @@
 #define RES_ITALIC_FONT     CINDER_RESOURCE(../resources/, isocpeui.ttf, 129, TFF)
 #define RES_BOLD_FONT       CINDER_RESOURCE(../resources/, isocpeur.ttf, 130, TFF)
 #define RES_DEFAULT_PROFILE CINDER_RESOURCE(../resources/, defaulticon.jpg, 131, IMAGE)
-#define RES_NODE_HIGHLIGHT  CINDER_RESOURCE(../resources/, blue_ring.png, 132, IMAGE)
 
 #define RES_CROWDTAP_FONT   CINDER_RESOURCE(../resources/, HouschkaPro-Medium.ttf, 133, TTF)
 #define RES_CROWDTAP_FONT_BOLD CINDER_RESOURCE(../resources/, HouschkaPro-Bold.ttf, 134, TTF)
