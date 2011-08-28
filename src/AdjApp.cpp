9baeac586c0dd8304bf8cad8802d5d4d6911b2fe
Patrick Tierney <patrick.l.tierney@gmail.com>
2011-08-28 21:06:29 +0100

    There's a bug with Cairo somewhere so I need to start this not in fullscreen.

--- src/AdjApp.cpp
+++ src/AdjApp.cpp
@@ -11,10 +11,10 @@ AdjApp::AdjApp() {
 }
 
 void AdjApp::prepareSettings(Settings* settings) {
-	//settings->setWindowSize(1024, 768);
+	settings->setWindowSize(1024, 768);
 	settings->setFrameRate(60.0f);
 	settings->setResizable(false);
-	settings->setFullScreen(true);
+	settings->setFullScreen(false);
 //	settings->setResizable(true);
 //	settings->setFullScreen(false);
 
